21f99de1f6e9e653a289d00cce4a4a68e265bb12
Katu <captain.katu@gmail.com>
2024-08-18 14:17:37 -0500

    cleaning out configs and dependency mod stuff

--- kubejs/server_scripts/sanity_effects.js
+++ kubejs/server_scripts/sanity_effects.js
@@ -6,20 +6,19 @@ PlayerEvents.tick(event => {
         if (sanity >= 0.6) {
             if(player.stages.has('insanity')) return;
                 player.stages.add('insanity')
-                player.tell("You feel a sense of well-being.")
+                player.tell("You feel as if the world is out to get you...")
                 event.server.runCommandSilent(`apathy set-admin join ${event.player.name.string} insane`)
     }
         if (sanity < 0.6) {
             if(!player.stages.has('insanity')) return;
                 player.stages.remove('insanity')
-                player.tell("You feel as if the world is out to get you...")
+                player.tell("You feel a sense of well-being.")
                 event.server.runCommandSilent(`apathy set-admin part ${event.player.name.string} insane`)
     }
 })
 
 PlayerEvents.tick(event => {
     let { player, level, server } = event
-    let sanity = player.nbt.ForgeCaps['sanitydim:sanity']['sanity.sanity']
     if (player.age % 200 != 0) return
     if (player.chestArmorItem.hasTag('forge:sanity_drain_armor')) {
         event.server.runCommandSilent(`sanity add ${event.player.name.string} -1`)
@@ -32,5 +31,17 @@ PlayerEvents.tick(event => {
     }
     if (player.feetArmorItem.hasTag('forge:sanity_drain_armor')) {
         event.server.runCommandSilent(`sanity add ${event.player.name.string} -1`)
+    }
+    if (player.hasEffect('born_in_chaos_v1:light_rampage')) {
+        event.server.runCommandSilent(`sanity add ${event.player.name.string} -3`)
+    }
+    if (player.hasEffect('born_in_chaos_v1:medium_rampage')) {
+        event.server.runCommandSilent(`sanity add ${event.player.name.string} -5`)
+    }
+    if (player.hasEffect('born_in_chaos_v1:strong_rampage')) {
+        event.server.runCommandSilent(`sanity add ${event.player.name.string} -7`)
+    }
+    if (player.hasEffect('born_in_chaos_v1:rampant_rampage')) {
+        event.server.runCommandSilent(`sanity add ${event.player.name.string} -10`)
     }
 })

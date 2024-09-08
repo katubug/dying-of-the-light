e12e767189f4cfeba6855580747bc295b6d7cf3e
Katu <captain.katu@gmail.com>
2024-09-08 18:31:43 -0500

    9-8-24
    - made some minor quest edits
    - finished up the major quest book pages (well, the ones i'd already made - probably still room for more)

--- kubejs/server_scripts/bad_deeds.js
+++ kubejs/server_scripts/bad_deeds.js
@@ -2,14 +2,14 @@ EntityEvents.hurt(event =>{
     let { player, entity, target, server, source } = event
     if (!source.actual.type) return
     //Don't Hit Babies
-    if (source.actual.type == 'minecraft:player' && entity.isBaby()){
+    if (source.actual.type == 'minecraft:player' && entity.nbt.Age < 0){
         console.log(`${source.getPlayer().name.string} is hurting a baby!`)
-        event.server.runCommandSilent(`/tbalignment ${source.getPlayer().name.string} decrease 10`)
+        event.server.runCommandSilent(`/tbalignment ${source.getPlayer().name.string} decrease 20`)
     }
-    /*
-    if (source.actual.type == 'minecraft:player' && entity.){}
-        console.log(`${source.getPlayer().name.string} is hurting a baby!`)
-        event.server.runCommandSilent(`/tbalignment ${source.getPlayer().name.string} decrease 10`)
+    
+    if (source.actual.type == 'minecraft:player' && entity.nbt.Owner){
+        console.log(`${source.getPlayer().name.string} is hurting a pet!`)
+        event.server.runCommandSilent(`/tbalignment ${source.getPlayer().name.string} decrease 20`)
     }
-        */
+        
 })
